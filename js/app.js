b87fbf32b3f9468e857ba792dbba9dfdcaea8fe6
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-05-11 15:27:47 -0400

    update darkmode with localStorage

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
+import {colorMode, checkLocalStorage, themeSwitcher} from './darkmode.js'
+
 // Nav Elements
 const development = document.getElementById('development');
 const design = document.getElementById('uidesign');
 const ccreation = document.getElementById('ccreation');
 const connect = document.getElementById('connect');
 const toplink = document.getElementById('backToTop');
-// Darkmode Elements
 const darkmodeTheme = document.getElementById('darkmode');
-const siteBody = document.querySelector('body');
 
 // Smooth Scrolling for Nav
 development.addEventListener('click', e => {
@@ -39,30 +39,6 @@ toplink.addEventListener('click', e => {
     document.getElementById('top').scrollIntoView({ behavior: 'smooth' });
 });
 
-// Darkmode Switching Functionality
-darkmodeTheme.addEventListener('click', e => {
-    if (siteBody.className === '') {
-        siteBody.className = 'darkmode';
-        darkmodeTheme.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sun" width="34" height="34" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <circle cx="12" cy="12" r="4" />
-            <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
-        </svg>
-        `;
-    } else if (siteBody.className === 'darkmode') {
-        siteBody.className = '';
-        darkmodeTheme.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moon-stars" width="34" height="34" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-            <path d="M17 4a2 2 0 0 0 2 2a2 2 0 0 0 -2 2a2 2 0 0 0 -2 -2a2 2 0 0 0 2 -2" />
-            <path d="M19 11h2m-1 -1v2" />
-        </svg>
-`;
-    }
-});
-
 // Closes the Nav when clicking on an item on mobile view
 const nav = document.querySelector('UL');
 const home = document.querySelector('#jr');
@@ -76,3 +52,6 @@ home.addEventListener('click', () => {
     let checkbox = document.querySelector('.nav-toggle');
     checkbox.checked = false;
 })
+
+//Set current localStorage
+checkLocalStorage()
